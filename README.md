bc164d48d67576bc12498f450a0bd71bfbdd9e1e
Victor Ibañez <vibanez18>
2021-02-07 18:39:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Micronaut 2.3.1 Documentation
+
+- [User Guide](https://docs.micronaut.io/2.3.1/guide/index.html)
+- [API Reference](https://docs.micronaut.io/2.3.1/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/2.3.1/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/index.html)
+---
+
+## Feature aws-alexa documentation
+
+- [Micronaut Alexa Skills documentation](https://micronaut-projects.github.io/micronaut-aws/latest/guide/index.html#alexa)
+
+## Feature aws-lambda documentation
+
+- [Micronaut AWS Lambda Function documentation](https://micronaut-projects.github.io/micronaut-aws/latest/guide/index.html#lambda)
+
---
+++ src/test/java/com/alexa/SessionEndedRequestIntentHandlerTest.java
@@ -0,0 +1,32 @@
+package com.alexa;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.RequestEnvelope;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.SessionEndedRequest;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest
+public class SessionEndedRequestIntentHandlerTest {
+
+    @Inject
+    SessionEndedRequestIntentHandler handler;
+
+    @Test
+    void testSessionEndedRequestIntentHandler() {
+        SessionEndedRequest request = SessionEndedRequest.builder().build();
+        HandlerInput input = HandlerInput.builder()
+                .withRequestEnvelope(RequestEnvelope.builder()
+                        .withRequest(request)
+                        .build()
+                ).build();
+        assertTrue(handler.canHandle(input));
+        Optional<Response> responseOptional = handler.handle(input);
+        assertTrue(responseOptional.isPresent());
+    }
+}
